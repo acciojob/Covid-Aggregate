2d1ebe49d3d4249f38757c7b0883e3609930e08b
Ratna Chakraborty <chakrabortysubhradeep556@gmail.com>
2022-05-26 16:13:19 +0530

    Add CreateDatabase, connector.js etc.

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-// This file is not to be modified. Please ignore this.
-// We will understand all of this later in the course.
-// DO NOT MODIFY THIS FILE
-
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
 const app = express();
+const { CovidTally } = require("./connector");
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname))
+app.get("/", (req, res) => {
+  res.status(200).send("HELLO WORLD");
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
+// your code goes here
+
+
 module.exports = app;
